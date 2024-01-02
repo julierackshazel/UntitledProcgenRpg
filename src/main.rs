f5fb0db7a5995c08cc4f8c2572d73c25333122bc
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-01-02 01:52:47 -0800

    yeah

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::{ecs::{component, query}, prelude::*};
+
+fn hello_world() {
+    println!("hello world");
+}
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_peoples(mut commands: Commands) {
+    commands.spawn((Person, Name("julie".to_string())));
+    commands.spawn((Person, Name("racks".to_string())));
+}
+
+fn greet_the_people(query: Query<>    
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, hello_world)
+        .run()
 }
