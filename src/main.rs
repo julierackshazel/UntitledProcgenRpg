bae3059cf6283c8e036bcd35528c7d32ffb953e8
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-01-02 23:48:39 -0800

    cargo.toml edit, REMOVE DYNAMIC LINKING BEFORE RELEASE

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{ecs::{component, query}, prelude::*};
+use bevy::prelude::*;
 
 fn hello_world() {
     println!("hello world");
@@ -14,10 +14,16 @@ fn add_peoples(mut commands: Commands) {
     commands.spawn((Person, Name("racks".to_string())));
 }
 
-fn greet_the_people(query: Query<>    
+fn greet_the_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}", name.0);
+    }
+}
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Update, hello_world)
-        .run()
+        .add_systems(Update, (hello_world, add_peoples, greet_the_people))
+        .run();
+    let at_the_disco = 3;
+    panic!("{}", at_the_disco);
 }
