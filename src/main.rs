c798a4021629345dd6e1477b592f6a5d36a27282
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-01-03 01:14:38 -0800

    first signed  change woo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ecs::query, prelude::*, time};
 
 fn hello_world() {
     println!("hello world");
@@ -9,21 +9,34 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(5.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_peoples)
+            .add_systems(Update, greet_the_people);
+    }
+}
 fn add_peoples(mut commands: Commands) {
     commands.spawn((Person, Name("julie".to_string())));
     commands.spawn((Person, Name("racks".to_string())));
 }
 
-fn greet_the_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_the_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Person>>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}", name.0);
+        }
     }
 }
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Update, (hello_world, add_peoples, greet_the_people))
-        .run();
-    let at_the_disco = 3;
-    panic!("{}", at_the_disco);
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
 }
