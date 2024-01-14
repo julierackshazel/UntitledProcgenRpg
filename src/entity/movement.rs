72facf998510bb258acc88b36ba2c54b029d9143
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-01-14 05:50:59 -0800

    yeah

--- src/entity/movement.rs
+++ src/entity/movement.rs
@@ -4,5 +4,7 @@ fn move_right(time: Res<time>, key: Res<Input<KeyCode>>, mut query: Query<&mut T
     sprite_bundle: SpriteBundle;
     if key.pressed(KeyCode::Right) {
         transform.translation.y += 150. * time.delta_seconds();
+    } else if key.pressed(KeyCode::Left) { 
+        transform.translation.y -= 150. * time.delta_seconds();
     }
 }
